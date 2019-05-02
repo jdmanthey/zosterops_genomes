6566ce694b1f4f3873b2b4c19730592f94941d6a
Joe Manthey <jdmanthey@gmail.com>
2019-05-02 16:20:32 -0500

    Create 02_bootstraps.sh

---
+++ 07_demography/02_bootstraps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N zost_msmc
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -pe sm 2
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+#$ -t 1-20
+
+for i in $( echo Z*/ ); do
+cd $i;
+cd bootstrap_${SGE_TASK_ID};
+../../msmc2_linux64bit -o bootstrap_${SGE_TASK_ID}_output -i 20 -t 6 -m 0.002166257 -p 1*2+20*1+1*2+1*3 *txt;
+cd ../../;
+done
