7a2ac52e35a8f165dbd87446226bb26cf3ad1c69
Joe Manthey <jdmanthey@gmail.com>
2020-12-11 11:35:23 -0600

    Update and rename 03_cut to 03_cut.sh

--- 04_depth/03_cut.sh
+++ 04_depth/03_cut.sh
@@ -1,3 +1,14 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N zost_cut
+#$ -q omni
+#$ -pe sm 2
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
 # break up the depth files into single column files for each individual (locations dropped)
 
 while read -r name1 number1; do
