61b9b46080a2ab50db1f422f62e954c85b180ace
Joe Manthey <jdmanthey@gmail.com>
2020-12-17 10:56:55 -0600

    Update and rename 03_create_bootstraps to 02_create_bootstraps.sh

--- 07_demography/02_create_bootstraps.sh
+++ 07_demography/02_create_bootstraps.sh
@@ -1,10 +1,10 @@
-cd ~/Desktop/msmc/
+cd /lustre/scratch/jmanthey/10_zosterops/zosterops_demography/
 
 # run the msmc utility script for each directory to create bootstrap replicates
 
 for i in $( echo Z*/ ); do 
 cd $i;
-../multihetsep_bootstrap.py -n 20 -s 1000000 --chunks_per_chromosome 30 --nr_chromosomes 31 \
+~/multihetsep_bootstrap.py -n 10 -s 1000000 --chunks_per_chromosome 30 --nr_chromosomes 31 \
 --seed 324324 bootstrap *txt;
 cd ..;
 done
