b54be0357f88ea0b413c1054cd42efc388e0560d
Joe Manthey <jdmanthey@gmail.com>
2020-12-18 09:58:59 -0600

    Update 07_combine_window_calcs.sh

--- 05_process_vcf/07_combine_window_calcs.sh
+++ 05_process_vcf/07_combine_window_calcs.sh
@@ -2,8 +2,8 @@ cd /lustre/scratch/jmanthey/10_zosterops/03_vcf/windows
 
 # combine the output for different analyses into a single file each
 # first add a header for each file
-grep 'pop1' CHR10:10000001-10050000__stats.txt > ../window_heterozygosity.txt
-grep 'pop1' CHR10:10000001-10050000__stats.txt > ../window_titv.txt
+grep 'pop1' CHR10:10000001-10025000__stats.txt > ../window_heterozygosity.txt
+grep 'pop1' CHR10:10000001-10025000__stats.txt > ../window_titv.txt
 
 # add the relevant stats to each file
 for i in $( ls *txt ); do grep 'heterozygosity' $i >> ../window_heterozygosity.txt; done
