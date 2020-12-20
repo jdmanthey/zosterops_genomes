07f362b25504935981ba1cc7d320ebd7bdf6ec18
Joe Manthey <jdmanthey@gmail.com>
2020-12-20 15:04:32 -0600

    Update and rename 06_summarize_trees to 04_summarize_trees.sh

--- 06_phylo/04_summarize_trees.sh
+++ 06_phylo/04_summarize_trees.sh
@@ -1,8 +1,8 @@
 # maximum clade credibility tree (simple summarization) from all gene trees using dendropy:
 # gives info about which nodes have support from what proportion of gene trees
-sumtrees.py --output=zost_summed.tre --min-clade-freq=0.05 zosterops_total.trees
+sumtrees.py --output=zost_summed.tre --min-clade-freq=0.05 zost_trees_25kbp.trees 
 
 # coalescent tree of all gene trees using ASTRAL III
 # automatically calculates local branch support using quartets, described here: https://doi.org/10.1093/molbev/msw079
-java -jar ~/Astral/astral.5.6.3.jar -i zosterops_total.trees -o zosterops_astral.tre 2> zosterops_astral.log
+java -jar ~/Astral/astral.5.6.3.jar -i zost_trees_25kbp.trees -o zosterops_astral.tre 2> zosterops_astral.log
 
